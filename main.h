d4510625cbaab6cbeb8828ce62aca61d3bf570f5
JayRolla <molapokgarose@gmail.com>
2024-01-30 11:19:12 -0800

    updated main.h

--- main.h
+++ main.h
@@ -3,9 +3,11 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.hi>
 /* prototypes */
 int _printf(const char *format, ...);
+int write_string_and_count(const char *str, int *count);
+void *extract_next_argument(va_list *args, char type);
 
 #endif /* MAIN_H */
 
